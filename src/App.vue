0940bdf88ddea28a55d0caf72243082321df7afe
Sebastian <sebasco015@gmail.com>
2023-10-23 17:35:59 -0500

    VueApp

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <h1>Rick and Morty</h1>
+    <FilterByStatus />
+    <FilterByName />
+    <ListCharacters />
+  </div>
+
+</template>
+
+<script>
+import FilterByName from './components/FilterByName'
+import FilterByStatus from './components/FilterByStatus'
+import ListCharacters from './components/ListCharacters'
+
+
+export default {
+  name: 'App',
+  components: {
+    ListCharacters,
+    FilterByStatus,
+    FilterByName
+  }
+}
+</script>
+
+<style lang="scss">
+:root {
+  --background-body: #24282F;
+  --background-card: #3C3E44;
+  --text-white: #FFFFFF;
+  --text-gray: #c0c0c0;
+  --text-orange: #FF9800;
+}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: var(--background-body);
+  color: var(--text-white);
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+h1 {
+  margin-bottom: 3rem;
+  text-align: center;
+}
+.container {
+  width: 980px;
+  max-width: 90%;
+  margin: 5rem auto;
+}
+img {
+  width: 100%;
+}
+</style>
